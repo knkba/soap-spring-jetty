d1b680c753050261df3f96d036c8975013aaced0
Carlos Andres Cano Barrios <50373001+knkba@users.noreply.github.com>
2023-06-14 11:26:57 -0500

    Initial commit

---
+++ src/main/java/com/example/demosoap/DemoSoapApplication.java
@@ -0,0 +1,14 @@
+package com.example.demosoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoSoapApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSoapApplication.class, args);
+    }
+
+}
